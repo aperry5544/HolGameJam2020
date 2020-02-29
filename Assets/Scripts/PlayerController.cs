42a16c898c0239c4ada30aac3fda105a1235c14f
Matthew Knight <matthew.knight@holovis.com>
2020-03-01 00:21:53 +0900

    Player Reflects off Surfaces

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -69,4 +69,21 @@ public class PlayerController : MonoBehaviour
     {
         shoulder.transform.Rotate(new Vector3(0, 0, 1), shoulderRotation * Time.deltaTime);
     }
+
+    private void Reflect(Vector2 tangent)
+    {
+        float angleOfIncidence = Vector2.SignedAngle(fist.transform.position - transform.position, tangent);
+
+        shoulder.transform.Rotate(new Vector3(0, 0, angleOfIncidence * 2));
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        ContactPoint2D contact = collision.GetContact(0);
+
+        Debug.DrawLine(contact.point, contact.point + (contact.normal*2), Color.red, 5);
+        Debug.DrawLine(contact.point, contact.point + (new Vector2(-contact.normal.y, contact.normal.x) * 2), Color.blue, 5);
+        
+        Reflect(new Vector2(-contact.normal.y, contact.normal.x));
+    }
 }

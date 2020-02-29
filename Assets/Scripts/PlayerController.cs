9935fbbc9ee342670da7912873dfe84de50d1ed1
Matthew Knight <matthew.knight@holovis.com>
2020-02-29 22:01:18 +0900

    Initial PlayerController

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject fist;
+
+    [SerializeField]
+    private float fistVelocity = 5;
+
+    [SerializeField]
+    private float fistRotation = 5;
+
+    private bool active = false;
+
+    public void Activate()
+    {
+        active = true;
+    }
+    public void Deactivate()
+    {
+        active = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(active)
+        {
+
+        }
+        else
+        {
+            RotateFist();
+        }
+    }
+
+    private void RotateFist()
+    {
+        fist.transform.Rotate(new Vector3(0, 0, 1), fistRotation * Time.deltaTime);
+    }
+}

8888d1b6d80e302249d02eff8dc1b4c97b8f75ec
Matthew Knight <matthew.knight@holovis.com>
2020-02-29 22:23:26 +0900

    Simple Player Movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
+    private GameObject shoulder;
+    [SerializeField]
     private GameObject fist;
 
     [SerializeField]
     private float fistVelocity = 5;
 
     [SerializeField]
-    private float fistRotation = 5;
+    private float shoulderRotation = 5;
+
+    [SerializeField]
+    private float fistActivePos = 0.5f;
+    [SerializeField]
+    private float fistInactivePos = 0.4f;
 
     private bool active = false;
+
+    [SerializeField]
+    private Vector3 fistDirection;
 
     public void Activate()
     {
@@ -26,9 +36,22 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            fist.transform.localPosition = new Vector3(0, fistActivePos, 0);
+            active = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            fist.transform.localPosition = new Vector3(0, fistInactivePos, 0);
+            active = false;
+        }
+
+
         if(active)
         {
-
+            FireFist();
         }
         else
         {
@@ -36,8 +59,14 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    private void FireFist()
+    {
+        fistDirection = fist.transform.position - transform.position;
+        gameObject.transform.Translate(fistDirection.normalized * fistVelocity * Time.deltaTime);
+    }
+
     private void RotateFist()
     {
-        fist.transform.Rotate(new Vector3(0, 0, 1), fistRotation * Time.deltaTime);
+        shoulder.transform.Rotate(new Vector3(0, 0, 1), shoulderRotation * Time.deltaTime);
     }
 }

62fb3747ca9b81cea9ada8c0d6b11fcf4eb4ebe8
Austin Perry <Austin.Perry@Holovis.com>
2020-03-01 04:35:46 +0900

    Add level transitions

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -33,9 +33,27 @@ public class GameManager : MonoBehaviour
     [SerializeField]
     private float gameStartTime = 5.0f;
 
+    [SerializeField]
+    private GameObject[] levels = null;
+
+    [SerializeField]
+    private float levelMoveDistance = 20;
+
+    [SerializeField]
+    private float levelMoveSpeed = 10;
+
+    [SerializeField]
+    private float playerRaiseScale = 1.5f;
+
+    [SerializeField]
+    private float playerRaiseScaleSpeed = 10.0f;
+
+    private int currentLevel = -1;
+    private int previousLevel = -1;
     private float currentGameStartTime = 0.0f;
     private float currentWelcome = 2.0f;
     private float currentFade = 2.0f;
+    private float currentPlayerScale = 1.0f;
 
     private Dictionary<KeyCode, PlayerController> playerList = null;
     private Dictionary<KeyCode, float> timeOfKeyUp = null;
@@ -62,6 +80,9 @@ public class GameManager : MonoBehaviour
         Welcome,
         Fade,
         Title,
+        LoadLevelPhase1,
+        LoadLevelPhase2,
+        LoadLevelPhase3,
         Gameplay,
     }
 
@@ -142,12 +163,21 @@ public class GameManager : MonoBehaviour
                 titleImage.enabled = true;
                 gameStartText.enabled = false;
                 break;
-            case GameState.Gameplay:
+            case GameState.LoadLevelPhase1:
                 splashImage.enabled = false;
                 splashText.enabled = false;
                 titleText.enabled = false;
                 titleImage.enabled = false;
                 gameStartText.enabled = false;
+                currentPlayerScale = 1;
+                BeginLoadLevel();
+                break;
+            case GameState.LoadLevelPhase2:
+                break;
+            case GameState.LoadLevelPhase3:
+                break;
+            case GameState.Gameplay:
+                FinishLoadLevel();
                 break;
             default:
                 break;
@@ -186,13 +216,19 @@ public class GameManager : MonoBehaviour
 
     private void OnGUI()
     {
-        if (CurrentGameState == GameState.Title)
-        { 
         Event e = Event.current;
-
-            // Handle players joining or leaving
-            if (e != null && e.isKey && e.keyCode != KeyCode.None)
+        
+        // Handle players joining or leaving
+        if (e != null && e.isKey && e.keyCode != KeyCode.None)
+        {
+            if (e.keyCode == KeyCode.Escape)
             {
+                Application.Quit();
+                return;
+            }
+
+            if (CurrentGameState == GameState.Title)
+            { 
                 if (e.type == EventType.KeyDown)
                 {
                     // If we are not an existing player, add myself to the roster
@@ -232,7 +268,7 @@ public class GameManager : MonoBehaviour
             currentGameStartTime -= Time.deltaTime;
             if (currentGameStartTime <= 0)
             {
-                UpdateState(GameState.Gameplay);
+                UpdateState(GameState.LoadLevelPhase1);
             }
 
             gameStartText.text = (((int)currentGameStartTime) + 1).ToString();
@@ -243,6 +279,155 @@ public class GameManager : MonoBehaviour
         {
             currentGameStartTime = gameStartTime;
             gameStartText.enabled = false;
+        }
+    }
+
+    private void BeginLoadLevel(int levelIndex = -1)
+    {
+        if (levelIndex == -1)
+        {
+            if (levels.Length < 3)
+            {
+                Debug.LogError("NEED AT LEAST 3 LEVELS. SELF DESTRUCTING NOW.");
+                UnityEditor.EditorApplication.isPlaying = false;
+            }
+            while (levelIndex == previousLevel || levelIndex == currentLevel || levelIndex == -1)
+            {
+                levelIndex = UnityEngine.Random.Range(0, levels.Length);
+            }
+        }
+
+        Debug.Log(string.Format("Chose Level {0}", levelIndex));
+
+        // Freeze Players
+        foreach (KeyValuePair<KeyCode, PlayerController> player in playerList)
+        {
+            // player.Value.Freeze();
+        }
+
+        // Disable current level walls
+        if (currentLevel > -1)
+        {
+            levels[currentLevel].GetComponent<Level>().DisableLevelPhysics(); 
+        }
+
+        // Enable the next level
+        levels[levelIndex].gameObject.SetActive(true);
+
+        // Diable physics on next level
+        if (levelIndex > -1)
+        {
+            levels[levelIndex].GetComponent<Level>().DisableLevelPhysics();
+        }
+
+        // Swap my current levels
+        previousLevel = currentLevel;
+        currentLevel = levelIndex;
+    }
+
+    private void FinishLoadLevel()
+    {
+        // Enable current level walls
+        if (currentLevel > -1)
+        {
+            levels[currentLevel].GetComponent<Level>().EnableLevelPhysics();
+        }
+
+        if (previousLevel > -1)
+        {
+            // Move and Disable the old level
+            levels[previousLevel].transform.position = new Vector3(levelMoveDistance, 0, 0);
+            levels[previousLevel].gameObject.SetActive(false);
+        }
+
+
+        // UnFreeze Players
+        foreach (KeyValuePair<KeyCode, PlayerController> player in playerList)
+        {
+            // player.Value.UnFreeze();
+        }
+    }
+
+    private void LoadLevelUpdate(int phase)
+    {
+        // Raise players
+        if (phase == 1)
+        {
+            currentPlayerScale += playerRaiseScaleSpeed * Time.deltaTime;
+
+            if (currentPlayerScale >= playerRaiseScale)
+            {
+                currentPlayerScale = playerRaiseScale;
+            }
+
+            foreach (KeyValuePair<KeyCode, PlayerController> player in playerList)
+            {
+                player.Value.transform.localScale = new Vector3(currentPlayerScale, currentPlayerScale, 1);
+            }
+
+            if (currentPlayerScale >= playerRaiseScale)
+            {
+                currentPlayerScale = playerRaiseScale;
+                UpdateState(GameState.LoadLevelPhase2);
+            }
+        }
+        // Move levels
+        else if (phase == 2)
+        {
+            // Update previous level
+            if (previousLevel != -1)
+            {
+                float newX = levels[previousLevel].transform.position.x - levelMoveSpeed * Time.deltaTime;
+                if (newX <= -levelMoveDistance)
+                {
+                    levels[previousLevel].transform.position = new Vector3(-levelMoveDistance, 0, 0);
+                    if (currentLevel > -1)
+                    {
+                        levels[currentLevel].transform.position = new Vector3(0, 0, 0);
+                    }
+                    UpdateState(GameState.LoadLevelPhase3);
+                    return;
+                }
+                levels[previousLevel].transform.position = new Vector3(newX, 0, 0);
+            }
+
+            // Update new level
+            if (currentLevel != -1)
+            {
+                float newX = levels[currentLevel].transform.position.x - levelMoveSpeed * Time.deltaTime;
+                if (newX <= 0)
+                {
+                    levels[currentLevel].transform.position = new Vector3(0, 0, 0);
+                    if (previousLevel > -1)
+                    {
+                        levels[previousLevel].transform.position = new Vector3(-levelMoveDistance, 0, 0);
+                    }
+                    UpdateState(GameState.LoadLevelPhase3);
+                    return;
+                }
+                levels[currentLevel].transform.position = new Vector3(newX, 0, 0);
+            }
+        }
+        // Lower players
+        else if (phase == 3)
+        {
+            currentPlayerScale -= playerRaiseScaleSpeed * Time.deltaTime;
+
+            if (currentPlayerScale <= 1)
+            {
+                currentPlayerScale = 1;
+            }
+
+            foreach (KeyValuePair<KeyCode, PlayerController> player in playerList)
+            {
+                player.Value.transform.localScale = new Vector3(currentPlayerScale, currentPlayerScale, 1);
+            }
+
+            if (currentPlayerScale <= 1)
+            {
+                currentPlayerScale = 1;
+                UpdateState(GameState.Gameplay);
+            }
         }
     }
 
@@ -263,6 +448,15 @@ public class GameManager : MonoBehaviour
                 break;
             case GameState.Title:
                 TitleUpdate();
+                break;
+            case GameState.LoadLevelPhase1:
+                LoadLevelUpdate(1);
+                break;
+            case GameState.LoadLevelPhase2:
+                LoadLevelUpdate(2);
+                break;
+            case GameState.LoadLevelPhase3:
+                LoadLevelUpdate(3);
                 break;
             case GameState.Gameplay:
                 GameplayUpdate();
---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    private Rigidbody2D[] allLevelRigidbodies = null;
+    private Collider2D[] allLevelColliders = null;
+
+    void Start()
+    {
+        allLevelRigidbodies = gameObject.GetComponentsInChildren<Rigidbody2D>();
+        allLevelColliders = gameObject.GetComponentsInChildren<Collider2D>();
+    }
+
+    public void DisableLevelPhysics()
+    {
+        if (allLevelRigidbodies != null)
+        {
+            for (int i = 0; i < allLevelRigidbodies.Length; ++i)
+            {
+                allLevelRigidbodies[i].isKinematic = true;
+            }
+        }
+
+        if (allLevelColliders != null)
+        {
+            for (int i = 0; i < allLevelColliders.Length; ++i)
+            {
+                allLevelColliders[i].enabled = false;
+            }
+        }
+    }
+
+    public void EnableLevelPhysics()
+    {
+        for (int i = 0; i < allLevelRigidbodies.Length; ++i)
+        {
+            allLevelRigidbodies[i].isKinematic = false;
+        }
+
+        for (int i = 0; i < allLevelColliders.Length; ++i)
+        {
+            allLevelColliders[i].enabled = true;
+        }
+    }
+}

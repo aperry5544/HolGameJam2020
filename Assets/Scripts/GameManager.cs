1b7cf41f5b2c7d1c4c14db5252fcc5624d54df99
Austin Perry <Austin.Perry@Holovis.com>
2020-03-02 00:15:25 +0900

    Add hurt sfx

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -16,7 +16,7 @@ public class GameManager : MonoBehaviour
     private Text titleText = null;
 
     [SerializeField]
-    private Image titleImage = null;
+    private SpriteRenderer titleImage = null;
 
     [SerializeField]
     private Text gameStartText = null;
